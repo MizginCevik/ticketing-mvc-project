86d46dffefbc6c3a6bedb00151c33ff40921d3df
mizgin <mizgincvk@gmail.com>
2022-09-30 13:06:59 -0400

    created DataGenerator and brought samples to Controller

--- src/main/java/com/cydeo/controller/UserController.java
+++ src/main/java/com/cydeo/controller/UserController.java
@@ -1,23 +1,33 @@
 package com.cydeo.controller;
 
 import com.cydeo.dto.UserDTO;
+import com.cydeo.service.RoleService;
+import com.cydeo.service.UserService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.util.List;
-
 @Controller
 @RequestMapping("/user")
 public class UserController {
+
+    private final RoleService roleService;
+    private final UserService userService;
+
+    public UserController(RoleService roleService, UserService userService) {
+        this.roleService = roleService;
+        this.userService = userService;
+    }
 
     @GetMapping("/create")
     public String createUser(Model model){
 
         model.addAttribute("user", new UserDTO());
 
-        //model.addAttribute("roles", ); //bring all roles from DB
+        model.addAttribute("roles", roleService.findAll()); //bring all roles from DB
+        model.addAttribute("users", userService.findAll());
+
         return "/user/create";
     }
 }
--- src/main/java/com/cydeo/service/CRUDService.java
+++ src/main/java/com/cydeo/service/CRUDService.java
@@ -4,7 +4,7 @@ import java.util.List;
 
 public interface CRUDService<T,ID> {
 
-    T save(T user);
+    T save(T object);
     T findById(ID id);
     List<T> findAll();
     void deleteById(ID id);
